e84b5c131265f152ac96a016e22d3e41adb8def0
Aditi Tiwari <115400068+Aditiwari7@users.noreply.github.com>
2023-05-28 18:54:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-let yourArray = ["adi", 1.2, 10, {a: "hi", b: 2345}, true];
-console.log(yourArray);
+// let yourArray = ["adi", 1.2, 10, {a: "hi", b: 2345}, true];
+// console.log(yourArray);
+
+const yourArray = [15,"Trimbak", true, 9, 2000];
